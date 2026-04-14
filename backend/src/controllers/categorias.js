481e8dd63883f1f2b8a7076e767864d7002421d7
xeroxbob27 <gui.lopes.1997@gmail.com>
2026-04-14 01:00:24 +0000

    (13/04) Término da implementação do CRUD de categorias

--- backend/src/controllers/categorias.js
+++ backend/src/controllers/categorias.js
@@ -54,5 +54,95 @@ controller.retrieveAll = async function(req, res) {
  }
 }
 
+controller.retrieveOne = async function(req, res) {
+ try {
+
+
+   // Manda recuperar o documento no servidor de BD
+   // usando como critério um id informado no parâmetro
+   // da requisição
+   const result = await prisma.categoria.findUnique({
+     where: { id: req.params.id }
+   })
+
+
+   // Encontrou o documento ~> retorna HTTP 200: OK (implícito)
+   if(result) res.send(result)
+   // Não encontrou o documento ~> retorna HTTP 404: Not Found
+   else res.status(404).end()
+ }
+ catch(error) {
+   // Algo deu errado: exibe o erro no terminal
+   console.error(error)
+
+
+   // Envia o erro ao front-end, com código de erro
+   // HTTP 500: Internal Server Error
+   res.status(500).send(error)
+ }
+}
+
+controller.update = async function(req, res) {
+ try {
+   // Busca o documento passado como parâmetro e, caso o documento seja
+   // encontrado, atualiza-o com as informações contidas em req.body
+   await prisma.categoria.update({
+     where: { id: req.params.id },
+     data: req.body
+   })
+
+
+   // Encontrou e atualizou ~> retorna HTTP 204: No Content
+   res.status(204).end()
+ }
+ catch(error) {
+   // Algo deu errado: exibe o erro no terminal
+   console.error(error)
+
+
+   // P2025: erro do Prisma referente a objeto não encontrado
+   if(error?.code === 'P2025') {
+     // Não encontrou e não atualizou ~> retorna HTTP 404: Not Found
+     res.status(404).end()
+   }
+   else {    // Outros tipos de erro
+     // Envia o erro ao front-end, com código de erro
+     // HTTP 500: Internal Server Error
+     res.status(500).send(error)
+   }
+ }
+}
+
+controller.delete = async function(req, res) {
+ try {
+   // Busca o documento pelo id passado como parâmetro
+   // e efetua a exclusão, caso o documento seja encontrado
+   await prisma.categoria.delete({
+     where: { id: req.params.id }
+   })
+
+
+   // Encontrou e excluiu ~> retorna HTTP 204: No Content
+   res.status(204).end()
+ }
+ catch(error) {
+   // Algo deu errado: exibe o erro no terminal
+   console.error(error)
+
+
+   // P2025: erro do Prisma referente a objeto não encontrado
+   if(error?.code === 'P2025') {
+     // Não encontrou e não excluiu ~> retorna HTTP 404: Not Found
+     res.status(404).end()
+   }
+   else {    // Outros tipos de erro
+     // Envia o erro ao front-end, com código de erro
+     // HTTP 500: Internal Server Error
+     res.status(500).send(error)
+   }
+ }
+}
+
+
 
 export default controller
